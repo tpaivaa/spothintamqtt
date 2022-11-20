081c195e84e48de38020b7da88f8e7eecde7c0d4
Tomi Hakkarainen <tomi.hakkarainen@advania.com>
2022-11-20 23:17:10 +0200

    no loop in args

--- main.py
+++ main.py
@@ -63,6 +63,6 @@ if __name__ == '__main__':
   loop = asyncio.new_event_loop()
   asyncio.set_event_loop(loop)
   try:
-    asyncio.run(run(loop=loop))
+    asyncio.run(run())
   except KeyboardInterrupt:
     pass
